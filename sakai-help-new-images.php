f55ab99e3ad5272b6614cf4b5c788049bf2484a5
Sam Ottenhoff <ottenhoff@longsight.com>
2014-04-30 09:49:22 -0400

    Script for finding new images in exported help

---
+++ sakai-help-new-images.php
@@ -0,0 +1,37 @@
+#!/usr/bin/php
+<?php
+
+$imagedir = "/tmp/help/images/";
+$dest = "/home/samo/dev/trunk-all/reference/library/src/webapp/image/help/en/";
+
+$arr = scandir ($imagedir);
+
+foreach ($arr AS $dir) {
+  if ($dir == "." || $dir == "..") continue;
+
+  $subdir = scandir ($imagedir . $dir);
+  foreach ($subdir AS $sub) {
+    if ($sub == "." || $sub == "..") continue;
+
+    if (!is_file($imagedir . $dir . "/" . $sub)) {
+      print "Skipping " . $imagedir . $dir . "/" . $sub . "\n";
+      continue;
+    }
+
+    if (!is_file ($dest . $dir . "/" .  $sub)) {
+      $cmd =  "optipng -o7  " . $imagedir . $dir . "/" . $sub;
+      var_dump ($cmd);
+      exec ($cmd);
+
+      $cmd =  "pngquant --ext .png --force 64 " . $imagedir . $dir . "/" . $sub;
+      var_dump ($cmd);
+      exec ($cmd);
+
+      if (!is_dir ($dest . $dir)) {
+        mkdir ($dest . $dir);
+      }
+      copy ($imagedir . $dir . "/" . $sub, $dest . $dir . "/". $sub);
+    }
+  }
+
+}
